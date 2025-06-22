ffde1397d13c1e1acbeaadfe0dd5d5b0115ec7c5
Uriah Finkel <Ufinkel@gmail.com>
2025-06-22 17:13:32 +0300

    fix: remove aalen_johansen from __init__.py

--- src/polarstate/__init__.py
+++ src/polarstate/__init__.py
@@ -1,6 +1,6 @@
 """Public API for the :mod:`polarstate` package."""
 
-from .aj import aalen_johansen, prepare_event_table
+from .aj import prepare_event_table
 from .predict import predict_aj_estimates
 
 __all__ = ["aalen_johansen", "prepare_event_table", "predict_aj_estimates"]
